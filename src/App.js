95be2affeecec6e73ec1d2d0ff36b59a032a7b33
Divine Olokor <divee789@gmail.com>
2021-03-08 09:15:37 +0100

    fixed deployment error in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import History from "./components/utils";
 import LandingPage from "./components/landingPage";
 import SignUp from "./components/signUp";
